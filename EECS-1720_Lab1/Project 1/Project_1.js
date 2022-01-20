76a0e6c2ce339f8cfbe077dc081ebcb0235abc93
LordEman1 <97703227+LordEman1@users.noreply.github.com>
2022-01-20 17:31:24 -0500

    Add files via upload

--- EECS-1720_Lab1/Project 1/Project_1.js
+++ EECS-1720_Lab1/Project 1/Project_1.js
@@ -1,5 +1,1 @@
-window.addEventListener('load', (event) => {
-    document.getElementById("inputText").addEventListener("change", event =>{
-        document.getElementById("headingText").innerHTML = event.target.value;
-    });
-});
+/*code*/
