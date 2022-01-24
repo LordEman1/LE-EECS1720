9b2a84dc363c2479748c717c673fe10047e5579e
LordEman1 <97703227+LordEman1@users.noreply.github.com>
2022-01-24 17:26:00 -0500

    Add files via upload

--- EECS-1720_Lab1/Project 1/Project_1.js
+++ EECS-1720_Lab1/Project 1/Project_1.js
@@ -1,14 +1,24 @@
-window.onload = function () {
-    document.getElementById("button").onclick = changeText();
-    document.getElementById("button1").onclick = removeOption();
+window.onclick = function () {
+    document.getElementById("button").onclick = changeText(generateName());
 }
 
-function changeText() {
+function changeText(name) {
     x = document.getElementById("mySelect");
-    x.options[x.selectedIndex].text = document.getElementById("inputText");
+    x.options[x.selectedIndex].text = name;
   }
 
-function removeOption() {
-    var x = document.getElementById("mySelect");
-    x.remove(x.selectedIndex);
+
+function getRandomInt(min, max) {
+  	return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateName(name){
+	var name1 = ["Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson", "Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller"];
+
+	var name2 = ["Smith", "Johnson", "Williams", "Brown", "Jones", "Garcia", "Miller", "Davis", "Rodriguez", "Martinez", "Hernandez", "Lopez", "Gonzalez", "Wilson"];
+
+	var name = name1[getRandomInt(0, name1.length)] + ' ' + name2[getRandomInt(0, name2.length)];
+	
+    return name;
+
 }
