3ed89f4552144803f5db90a69f25c45548e1d610
LordEman1 <97703227+LordEman1@users.noreply.github.com>
2022-01-24 15:36:57 -0500

    Add files via upload

--- EECS-1720_Lab1/Project 1/Project_1.js
+++ EECS-1720_Lab1/Project 1/Project_1.js
@@ -1,1 +1,14 @@
-/*code*/
+window.onload = function () {
+    document.getElementById("button").onclick = changeText();
+    document.getElementById("button1").onclick = removeOption();
+}
+
+function changeText() {
+    word = document.getElementById("button");
+    word.options[word.selectedIndex].text = "word";
+  }
+
+function removeOption() {
+    var word = document.getElementById("button1");
+    word.remove(word.selectedIndex);
+}
