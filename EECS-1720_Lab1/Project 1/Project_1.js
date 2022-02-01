c128f4ec4b2cfabd7915aaef02b456a0597ac11c
LordEman1 <97703227+LordEman1@users.noreply.github.com>
2022-01-31 22:49:02 -0500

    Add files via upload

--- EECS-1720_Lab1/Project 1/Project_1.js
+++ EECS-1720_Lab1/Project 1/Project_1.js
@@ -1,13 +1,37 @@
-/*window.onclick = function () {
-    document.getElementById("button").onclick = changeText(generateName());
-}*/
+const colorEl = document.getElementById("color-changer");
+const rotateEl = document.getElementById("rotate");
+const rotate3DEl = document.getElementById("rotate3d");
 
-function changeText(name) {
-    x = document.getElementById("mySelect");
-    x.options[x.selectedIndex].text = name;
-  }
+colorEl.addEventListener("input", () => {
+    injectCSS(`body {
+        background-color: ${colorEl.value}!important;
+    }`);
+});
+
+rotateEl.addEventListener("input", () => {
+    injectCSS(`body {
+        transform: rotate(${rotateEl.value}deg)!important;
+    }`);
+});
+
+rotate3DEl.addEventListener("input", () => {
+    injectCSS(`body {
+        transform: rotate3d(1,1,1,${rotate3DEl.value}deg)!important;
+    }`);
+});
+
+function injectCSS(css) {
+  chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
+    if (!tab) return;
+    chrome.scripting.insertCSS({
+      css,
+      target: { tabId: tab.id },
+    });
+  });
+}
 
 
+/*
 function getRandomInt(min, max) {
   	return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -22,3 +46,4 @@ function generateName(name){
     return name;
 
 }
+*/
